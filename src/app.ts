48a19c92d4f8a36625981e90d0bc74bbb0d47bb8
Paulo-Pacheco-Junior <paulojrgyn88@gmail.com>
2025-07-11 10:59:15 -0300

    refactor: update app configuration

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "express-async-errors";
 
+import { routes } from "./routes";
 import { errorHandling } from "./middlewares/error-handling";
 
 const app = express();
@@ -10,6 +11,8 @@ app.use(express.json());
 
 app.use(cors());
 
+app.use(routes);
 
 app.use(errorHandling);
+
 export { app };
