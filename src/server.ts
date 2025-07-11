3bf67392fdccb04f3a649da6d3b62e482a464681
Paulo-Pacheco-Junior <paulojrgyn88@gmail.com>
2025-07-11 10:58:24 -0300

    refactor: move dotenv config to env.ts and clean up server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,7 @@
-import 'dotenv/config';
-import { Request, Response } from 'express';
 import { app } from "./app"
 import { env } from "./env"
 
 const PORT = env.PORT
-
-app.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'Sales Manager API is running!' });
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
